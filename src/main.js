0f8e213a259e948173dd8d7332234982f1429fe8
AustinMahan <amahan0424@gmail.com>
2016-07-26 12:10:58 -0600

    unique options

--- src/main.js
+++ src/main.js
@@ -1,23 +1,39 @@
 $(document).ready(function() {
+  $(document).submit(function(e){
+    e.preventDefault()
+  })
+
   $('button').click(function(){
-    findMovie($('input').val())
+    if($('input').val()){
+      findMovie($('input').val())
+
+    }
   })
 })
 
-function findMovie(movie){
-  // $.ajax({
-  //   url:'http://www.omdbapi.com/?t=' + movie,
-  //   method: 'get'
-  // }).done(function(data){
-  //   console.log(data);
-  // })
 
+var arrOfGenre = []
+
+function findMovie(movie){
   var promise = Promise.resolve($.ajax({
     url:'http://www.omdbapi.com/?t=' + movie,
     method: 'get'
   }))
   .then(function(data){
-    $('img').attr('src', data.poster)
-    $('.title').append('<h2>'+ data.title + '</h2>')
+    console.log(data);
+    $('.title').append('<div class="pics" style="float:left"><img src="' + data.Poster + '"/><h2>'+ data.Title + '</h2></div>')
+    arrOfGenre = arrOfGenre.concat(data.Genre.split(', '))
+    arrOfGenre = arrOfGenre.filter(function(item, pos, self){
+      return self.indexOf(item) == pos;
+    })
+    console.log(arrOfGenre);
+    $('.selectpicker').empty()
+    for(var i = 0; i < arrOfGenre.length; i++){
+      $('.selectpicker').append('<option val="' + arrOfGenre[i] + '">' + arrOfGenre[i] + '</option>')
+    }
   })
+}
+
+function addOptions(data){
+
 }
