b2739c23d7cac44c16180ee4113b0e51c16cdbaf
AustinMahan <amahan0424@gmail.com>
2016-07-26 09:48:03 -0600

    added ajax call

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-  console.log('test');
+  $('button').click(function(){
+
+  })
+
+
+findMovie('frozen')
 })
+
+function findMovie(movie){
+  // $.ajax({
+  //   url:'http://www.omdbapi.com/?t=' + movie,
+  //   method: 'get'
+  // }).done(function(data){
+  //   console.log(data);
+  // })
+  var promise = Promise.resolve($.ajax({
+    url:'http://www.omdbapi.com/?t=' + movie,
+    method: 'get'
+  }))
+  .then(function(data){
+    return data
+  })
+  .then(function(movie){
+    console.log(movie.Title);
+  })
+}
